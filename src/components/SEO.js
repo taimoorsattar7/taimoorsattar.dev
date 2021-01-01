790e9660ee15c3a1e0a1ca3a652c40017eb2cc3a
taimoorsattar7 <taimoorsattar7@gmail.com>
2021-01-01 22:32:27 +0300

    seo meta

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -4,107 +4,121 @@ import { StaticQuery, graphql } from "gatsby"
 
 import logo from "../images/logo.png"
 
-const SEO = ({ id,
-  url,
-  title,
-  description,
-  keywords,
-  image,
-  date,
-  modifiedDate,
-  schemaType }) => (
-    <StaticQuery
-      query={query}
-      render={({
-        site: {
-          siteMetadata: {
-            defaultTitle,
-            defaultDescription,
-            defaultKeywords,
-            defaultURL,
-            defaultAuthor
-          },
+const SEO = (
+  { id,
+    url,
+    title,
+    description,
+    keywords,
+    image,
+    date,
+    modifiedDate,
+    schemaType }) => (
+  <StaticQuery
+    query={query}
+    render={({
+      site: {
+        siteMetadata: {
+          defaultTitle,
+          defaultDescription,
+          defaultKeywords,
+          defaultURL,
+          defaultAuthor
         },
-      }) => {
+      },
+    }) => {
 
-        const main_schema = {
-          "@context": "http://schema.org",
+      const main_schema = {
+        "@context": "http://schema.org",
+        "@type": "Person",
+        "name": "Taimoor Sattar",
+        "url": "https://taimoorsattar.dev",
+        "sameAs": [
+          "https://www.linkedin.com/in/taimoorsattar",
+          "https://twitter.com/taimoorsattar7"
+        ]
+      }
+
+      const blog_schema = {
+        "@context": "http://schema.org",
+        "@type": "BlogPosting",
+        "@id": id,
+        "headline": title || defaultTitle,
+        "description": description || defaultDescription,
+        "thumbnailUrl": image || logo,
+        "author": defaultAuthor,
+        "datePublished": date,
+        "dateModified": date,
+        "image": image || logo,
+        "publisher": [{
           "@type": "Person",
-          "name": "Taimoor Sattar",
-          "url": "https://taimoorsattar.dev",
-          "sameAs": [
-            "https://www.linkedin.com/in/taimoorsattar",
-            "https://twitter.com/taimoorsattar7"
-          ]
-        }
+          "name": defaultAuthor || "Taimoor Sattar"
+        }],
+        "mainEntityOfPage": url
+      }
 
-        const blog_schema = {
-          "@context": "http://schema.org",
-          "@type": "BlogPosting",
-          "@id": id,
-          "headline": title || defaultTitle,
-          "description": description || defaultDescription,
-          "thumbnailUrl": image || logo,
-          "author": defaultAuthor,
-          "datePublished": date,
-          "dateModified": date,
-          "image": image || logo,
-          "publisher": [{
-            "@type": "Person",
-            "name": defaultAuthor || "Taimoor Sattar"
-          }],
-          "mainEntityOfPage": url
-        }
+      const seo = {
+        title: title || defaultTitle,
+        description: description || defaultDescription,
+        keywords: keywords || defaultKeywords,
+        image: image ? (window.location.origin + image) : "",
+        url: window.location.origin + window.location.pathname
+      }
 
-        const seo = {
-          title: title || defaultTitle,
-          description: description || defaultDescription,
-          keywords: keywords || defaultKeywords,
-          image: image,
-        }
-        return (
-          <Helmet>
+      return (
+        <Helmet>
 
-            <script type="application/ld+json">
-              {JSON.stringify(main_schema)}
-            </script>
+          <script type="application/ld+json">
+            {JSON.stringify(main_schema)}
+          </script>
 
-            {
-              schemaType === "blog" ?
-                (
-                  <script type="application/ld+json">
-                    {JSON.stringify(blog_schema)}
-                  </script>
-                ) : ""
-            }
+          {
+            schemaType === "blog" ?
+              (
+                <script type="application/ld+json">
+                  {JSON.stringify(blog_schema)}
+                </script>
+              ) : ""
+          }
 
-            <title>{seo.title}</title>
-            <meta property="title" content={seo.title} />
-            <meta property="og:title" content={seo.title} />
+          <title>{seo.title}</title>
+          <meta property="title" content={seo.title} />
+          <meta name="image" content={seo.image} />
+          <meta name="description" content={seo.description} />
+          <meta name="keywords" content={seo.keywords} />
+          <link rel="canonical" href={seo.url} />
+          <meta name="robots" content="index,follow" />
+          <html lang="en" />
 
-            <meta property="og:site_name" content="Taimoor Sattar" />
-            <meta name="image" content={seo.image} />
 
-            <meta name="description" content={seo.description} />
-            <meta property="og:description" content={seo.description} />
+          <meta property='og:type' content='article' />
+          <meta property='og:locale' content='en_US' />
+          {/* <meta property='og:image:height' content='630' />
+          <meta property='og:image:width' content='1200' /> */}
+          <meta property='og:site_name' content='Taimoor Sattar' />
+          <meta property='og:title' content={seo.title} />
+          <meta property='og:description' content={seo.description} />
+          <meta property='og:url' content={seo.url} />
 
-            <meta name="keywords" content={seo.keywords} />
-            <meta name="robots" content="index,follow" />
-            <html lang="en" />
+          <meta property='og:image' content={seo.image} />
+          <meta property='og:og:image:alt' content={seo.title} />
 
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@taimoorsattar7" />
 
-            <meta name="twitter:image" content={image} />
-            <meta property="og:image" content={image} />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:url' content={seo.url} />
+          <meta name='twitter:title' content={seo.title} />
+          <meta name='twitter:description' content={seo.description} />
 
-            <meta property="fb:app_id" content="217416985985327" />
+          <meta name='twitter:image' content={seo.image} />
+          <meta name='twitter:image:alt' content={seo.title} />
 
-          </Helmet>
-        )
-      }}
-    />
-  )
+          <meta property="fb:app_id" content="217416985985327" />
+
+        </Helmet>
+      )
+    }}
+  />
+)
 
 export default SEO
 
