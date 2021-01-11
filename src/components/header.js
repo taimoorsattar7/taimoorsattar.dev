da2d6a31d1c2a44edea2b72dbc76485f11d266ce
taimoorsattar7 <taimoorsattar7@gmail.com>
2021-01-11 21:49:21 +0300

    add sponsored link

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from "gatsby"
 
 import './_site-header.scss'
@@ -7,6 +7,7 @@ import './_wrapper.scss'
 import './_headline.scss'
 import './_primary-nav.scss'
 import './_sect.scss'
+import './_page.scss'
 
 import logo from "../images/logo.svg"
 import send from "../images/send.svg"
@@ -20,65 +21,80 @@ const Header = () => {
 
     const [hamBurger, handleHamBurger] = useState(true);
 
-    return (<header className="site-header">
-            <div className={"wrapper wrapper--narrow" +
-                            (hamBurger ? "" : " wrapper--no-bottom-padd") }>
-
-                <h1 className="headline headline--logo">
-                    <Link to="/">
-                        <img className="site-header__logo" src={logo} alt="Taimoor Sattar" />
-                        <span className="site-header__logo-text">Taimoor Sattar</span>
-                    </Link>
-                </h1>
-
-                <div 
-                    onClick={() => handleHamBurger(!hamBurger)} 
-                    className={hamBurger    ? "site-header__menu-icon" 
-                                        : "site-header__menu-icon--close-x"}>
-
-                    <div className="site-header__menu-icon__middle"></div>
-
+    return (
+        <>
+            <div className="
+                page__bg-black page__p-bottom-m page__p-top-m
+                headline--mid">
+                <div className="wrapper wrapper--narrow">
+                    ⚡<a rel="sponsored"
+                        href="https://keynotes.dev/c/build-profitable-websites"
+                        target="_blank"
+                        className="headline headline__text headline--white">
+                        Enroll "Build Profitable Websites" course
+                    </a>⚡
                 </div>
-
-                <div className="site-header__logo-send">
-                    <a href="mailto:taimoorsattar7@gmail.com">
-                        <img className="site-header__logo" src={send} alt="Taimoor Sattar" />
-                    </a>
-                </div>
-
-                <nav className= {"primary-nav primary-nav--pull-right"
-                                +  (hamBurger ? " primary-nav--vanish" : "") } >
-
-                    <ul className="nav">
-
-                        <li>
-                            <Link to="/about/">
-                                <img src={about} alt="Taimoor Sattar" />
-                                <span>About</span>
-                            </Link>
-                        </li>
-
-                        <li>
-                            <Link to="/blogs/">
-                                <img src={blogs} alt="Taimoor Sattar" />
-                                <span>Blogs</span>
-                            </Link>
-                        </li>
-
-                        <li>
-                            <Link to="/contact/">
-                                <img src={contact} alt="Taimoor Sattar" />
-                                <span>Contact</span>
-                            </Link>
-                        </li>
-
-                    </ul>
-
-                </nav>
-
-
             </div>
-        </header>);
+            <header className="site-header">
+                <div className={"wrapper wrapper--narrow" +
+                    (hamBurger ? "" : " wrapper--no-bottom-padd")}>
+
+                    <h1 className="headline headline--logo">
+                        <Link to="/">
+                            <img className="site-header__logo" src={logo} alt="Taimoor Sattar" />
+                            <span className="site-header__logo-text">Taimoor Sattar</span>
+                        </Link>
+                    </h1>
+
+                    <div
+                        onClick={() => handleHamBurger(!hamBurger)}
+                        className={hamBurger ? "site-header__menu-icon"
+                            : "site-header__menu-icon--close-x"}>
+
+                        <div className="site-header__menu-icon__middle"></div>
+
+                    </div>
+
+                    <div className="site-header__logo-send">
+                        <a href="mailto:taimoorsattar7@gmail.com">
+                            <img className="site-header__logo" src={send} alt="Taimoor Sattar" />
+                        </a>
+                    </div>
+
+                    <nav className={"primary-nav primary-nav--pull-right"
+                        + (hamBurger ? " primary-nav--vanish" : "")} >
+
+                        <ul className="nav">
+
+                            <li>
+                                <Link to="/about/">
+                                    <img src={about} alt="Taimoor Sattar" />
+                                    <span>About</span>
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link to="/blogs/">
+                                    <img src={blogs} alt="Taimoor Sattar" />
+                                    <span>Blogs</span>
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link to="/contact/">
+                                    <img src={contact} alt="Taimoor Sattar" />
+                                    <span>Contact</span>
+                                </Link>
+                            </li>
+
+                        </ul>
+
+                    </nav>
+
+
+                </div>
+            </header>
+        </>);
 };
 
 export default Header;
