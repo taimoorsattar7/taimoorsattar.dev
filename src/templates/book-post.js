a49d929a75f374c604760c79fd774f784f57c8e7
taimoorsattar7 <taimoorsattar7@gmail.com>
2021-04-24 22:32:59 +0300

    bold text

--- src/templates/book-post.js
+++ src/templates/book-post.js
@@ -1,28 +1,27 @@
-import React from 'react';
+import React from "react"
 
 import Image from "gatsby-image"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Pricetable from '../components/Pricetable';
+import Pricetable from "../components/Pricetable"
 
-import { graphql } from 'gatsby';
-import '../components/_blog-post.scss';
-import '../components/_social.scss';
-import '../components/_img.scss';
-import '../styles/_btn.scss';
+import { graphql } from "gatsby"
+import "../components/_blog-post.scss"
+import "../components/_social.scss"
+import "../components/_img.scss"
+import "../styles/_btn.scss"
 
 const BookPost = ({ data, location }) => {
-  const url = typeof window !== 'undefined' ? window.location.href : '';
-  const post = data.markdownRemark;
-  let featureImg = post.frontmatter?.featuredimage?.childImageSharp?.fluid;
+  const url = typeof window !== "undefined" ? window.location.href : ""
+  const post = data.markdownRemark
+  let featureImg = post.frontmatter?.featuredimage?.childImageSharp?.fluid
 
   const avatar = data?.avatar?.childImageSharp?.fixed
 
   return (
     <Layout location={location}>
-
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
@@ -32,31 +31,29 @@ const BookPost = ({ data, location }) => {
       <div className="wrapper wrapper--narrow">
         <div className="blogPost">
           <h1 className="headline">
-            {post.frontmatter.title}
+            <b>{post.frontmatter.title}</b>
           </h1>
 
           <div className="blogPost__info">
+            <span className="headline headline__sml headline--r-margin-sml">
+              By
+            </span>
 
-          <span className="headline headline__sml headline--r-margin-sml">
-          By
-          </span>
-
-          {avatar && (
-            <Image
-              fixed={avatar}
-              className="img__margin-r"
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            />
-          )}
+            {avatar && (
+              <Image
+                fixed={avatar}
+                className="img__margin-r"
+                imgStyle={{
+                  borderRadius: `50%`,
+                }}
+              />
+            )}
 
             <span className="headline headline__sml">
               <a className="headline--no-decor" href="/">
                 Taimoor Sattar
-                </a>
+              </a>
             </span>
-
           </div>
 
           <p className="headline headline__text headline--b-margin-medium headline--dull">
@@ -68,17 +65,14 @@ const BookPost = ({ data, location }) => {
               <button className="btn btn__slice btn--ctr-Atmed btn__r-margin">
                 <span className="headline headline__text headline--b">
                   BUY THE BOOK
-                  </span>
+                </span>
               </button>
             </a>
-
           </div>
 
-          {
-            featureImg && (
-              <Image className="blogPost__img-main" fluid={featureImg} />
-            )
-          }
+          {featureImg && (
+            <Image className="blogPost__img-main" fluid={featureImg} />
+          )}
 
           <div
             className="headline headline__text"
@@ -94,27 +88,24 @@ const BookPost = ({ data, location }) => {
         <Pricetable pricing={post.frontmatter?.pricing} />
 
         <div className="blogscontent__socialite">
-
           <iframe
             title="Substack"
             src="https://taimoor.substack.com/embed"
             width="480"
             height="320"
             Style={
-              'width: 100%;display:block;border:1px solid #EEE;background:white;margin:auto;margin-top: 50px;'
+              "width: 100%;display:block;border:1px solid #EEE;background:white;margin:auto;margin-top: 50px;"
             }
             frameborder="0"
             scrolling="no"
           ></iframe>
         </div>
-
-
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default BookPost;
+export default BookPost
 
 export const query = graphql`
   query {
@@ -128,7 +119,7 @@ export const query = graphql`
     markdownRemark(fields: { slug: { regex: "/books/" } }) {
       html
       frontmatter {
-        title        
+        title
         exerpt
         featuredimage {
           childImageSharp {
@@ -153,4 +144,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
